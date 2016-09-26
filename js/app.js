4ba9d2a28d01831cb1859a865f25c58969661080
Deepak <deepakssn@gmail.com>
2016-09-26 16:28:16 +0530

    Basic View

---
+++ js/app.js
@@ -0,0 +1,8 @@
+console.log("Running Skidta Careers - Deepak Radhakrishnan");
+$(document).ready(function() {
+    $("#frm-otp").hide();
+    $("#btn-email").click(function(){
+        $("#frm-email").hide();
+        $("#frm-otp").show();
+    });
+});
