2a9c0d37fe5dc9b0d950af21e5ecd79fc5766bb1
deepakssn <deepakssn@gmail.com>
2016-10-03 09:17:39 +0530

    Send default post values to test functionality

--- js/app.js
+++ js/app.js
@@ -2,11 +2,8 @@ console.log("Running Skidta Careers - Deepak Radhakrishnan");
 $(document).ready(function() {
     $("#frm-otp").hide();
     $("#btn-email").click(function() {
-        $.post( "http://127.0.0.1:8081/email", { email: "2pm" } );
-        // $.get("http://localhost:8081/domains", function(data) {
-        //     $(".result").html(data);
-        //     alert("Load was performed.");
-        // });
+      //TEST POST CALL
+        $.post( "http://127.0.0.1:8081/email", { email: "deepak@infosys.com" } );
         $("#frm-email").hide();
         $("#frm-otp").show();
     });
